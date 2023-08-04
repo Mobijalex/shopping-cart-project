95b87db468198ac0a92a32f2282c69c85bf524ed
mobijalex <mobijalex6@gmail.com>
2023-08-04 18:09:02 +0530

    1:33

--- main.js
+++ main.js
@@ -31,12 +31,15 @@ let shopItemsData = [
   },
 ];
 
+let basket = [];
+
 // this function allows us to generate the cards from the given data from the arrey useing map function
 
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let { id, name, price, desc, img } = x;
+      console.log(id);
       return `
     <div id = product-id-${id}class="item">
         <img width="220" src=${img} alt="" />
@@ -46,9 +49,9 @@ let generateShop = () => {
           <div class="price-quantity">
             <h2>$ ${price}</h2>
             <div class="buttons">
-              <i onclick="decremen()" class="bi bi-dash-lg"></i>
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
               <div id=${id} class="quantity">0</div>
-              <i onclick="increment()" class="bi bi-plus-lg"> </i>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"> </i>
             </div>
           </div>
         </div>
@@ -61,6 +64,43 @@ generateShop();
 
 // increment function and decrement function
 
-let increment = () => {};
-let decrement = () => {};
-let update = () => {};
+let increment = (item) => {
+  let selectedItem = item;
+
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  update(selectedItem.id);
+};
+
+// decrement function
+
+let decrement = (item) => {
+  let selectedItem = item;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  // console.log(basket);
+  update(selectedItem.id);
+};
+
+// updating the number from the increment and decrement in the card
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+// for updating the item number on the number on the basket
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+};
